7438fd6a9564c0785077d1b57614ec7a5fc8aa0f
Ferrokwastaken <israelduquesilva@hotmail.com>
2024-11-21 18:24:23 +0000

    validación

--- validation/validation.js
+++ validation/validation.js
@@ -4,6 +4,11 @@ form.addEventListener('submit', (event) => {
   event.preventDefault()
   let isValid = true
 
+  if ([...form.elements['topic[]']].filter(t => t.checked).length < 2) {
+    isValid = false
+    form.elements['topic[]'].setCustomValidity('Tienes que elegir al menos 2')
+  }
+
   if (isValid) {
     console.log('Submitting form...')
   }
@@ -16,5 +21,17 @@ form.addEventListener('input', (event) => {
     } else {
       event.target.nextElementSibling.textContent = event.target.validationMessage
     }
+  }
+})
+
+document.getElementById('email').addEventListener('input', (e) => {
+  let mail = form.elements['email'].value
+  let domain = mail.split('@')[1] ?? ''
+  if (domain.length <= 3) {
+    isValid = false
+    form.elements['email'].setCustomValidity('El dominio debe tener 3 o más carácteres')
+    document.getElementById('emailError').textContent = 'El dominio debe tener 3 o más carácteres'
+  } else {
+    form.elements['email'].setCustomValidity('')
   }
 })
